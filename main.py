2ad81c4271da23cfe2d2e2ed3d944b954ec84037
jlmarrugom <jlmarrugom@unal.edu.co>
2023-01-04 15:24:32 -0500

    feat: better app aesthetics

--- main.py
+++ main.py
@@ -4,14 +4,29 @@ import whisper
 from io import BytesIO
 import soundfile as sf
 
-audio_bytes = audio_recorder()
+st.title("Whisper App :loudspeaker:")
+st.write(
+    """
+    This app is a mix of [OpenAI's Whisper Model](https://github.com/openai/whisper), and a custom
+    Streamlit component that [records audio](https://github.com/Joooohan/audio-recorder-streamlit) Online.
 
-if audio_bytes:
+    Currently the app shows great results on Spanish and English Languages.
+
+    We used the Tiny version of the Whisper Model.
+    """)
+audio_bytes = audio_recorder(
+    pause_threshold= 2
+)
+try:
     data, samplerate = sf.read(BytesIO(audio_bytes))
     sf.write("audio.wav",data,samplerate)
     st.audio(audio_bytes, format = "audio/wav")
-
+except:
+    st.warning("Recorded Audio is too short, try again :relieved:")#wink
+if data.shape[0]>=10000:
     model = whisper.load_model("tiny")
     #result = model.transcribe(((data[:,0]+data[:,1])/2.0).astype(np.float32))
     result = model.transcribe("audio.wav")
-    st.write(result)
+    st.info(result["text"])
+
+else: st.warning("Recorded Audio is too short, try again :relieved:")#wink
