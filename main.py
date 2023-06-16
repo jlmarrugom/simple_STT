e09dddbe34edf78dbc65b42f79e844ecb3f48ea2
José Luis Marrugo Madrid <61395538+jlmarrugom@users.noreply.github.com>
2023-06-16 18:34:07 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from audio_recorder_streamlit import audio_recorder
 import whisper
 from io import BytesIO
 import soundfile as sf
+from tempfile import NamedTemporaryFile
+
+
+
 st.set_page_config(page_title="Whisper app", page_icon=":notes:")
 st.title("Whisper App :loudspeaker:")
 st.write(
@@ -14,18 +18,30 @@ st.write(
 
     We used the Tiny version of the Whisper Model.
     """)
-audio_bytes = audio_recorder(
-    pause_threshold= 2
-)
-try:
-    data, samplerate = sf.read(BytesIO(audio_bytes))
-    sf.write("audio.wav",data,samplerate)
-    st.audio(audio_bytes, format = "audio/wav")
-    if data.shape[0]>=10000:
-        model = whisper.load_model("tiny")
-        result = model.transcribe("audio.wav")
+
+uploaded_audio = st.file_uploader("Upload your audio file")
+if uploaded_audio is None:
+    audio_bytes = audio_recorder(
+        pause_threshold= 2
+    )
+    try:
+        data, samplerate = sf.read(BytesIO(audio_bytes))
+        sf.write("audio.wav",data,samplerate)
+        st.audio(audio_bytes, format = "audio/wav")
+        if data.shape[0]>=10000:
+            model = whisper.load_model("small") #tiny"
+            result = model.transcribe("audio.wav")
+            st.info(result["text"])
+
+        else: st.warning("Recorded Audio is too short, try again :relieved:")#wink
+    except:
+        st.info("Try to record or upload some audio :relieved:")
+else:
+    with NamedTemporaryFile(suffix="m4a") as temp:
+        temp.write(uploaded_audio.getvalue())
+        temp.seek(0)
+        model = whisper.load_model("small") #tiny"
+        result = model.transcribe(temp.name)
         st.info(result["text"])
 
-    else: st.warning("Recorded Audio is too short, try again :relieved:")#wink
-except:
-    st.info("Try to record some audio :relieved:")
+
